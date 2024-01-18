c405f9dcabc3f015aa8ae42a139a26134c2e8cec
Aaron <aaron012674@gmail.com>
2024-01-18 14:41:18 -0500

    Cleaned up code

--- src/engine/engineDriver.java
+++ src/engine/engineDriver.java
@@ -4,12 +4,8 @@ import engine.forGUI.Table;
 
 /*** The main client for the chessEngine package. */
 public class engineDriver {
-
   public static void main(String[] args) {
-
       Table.get().show();
-  
   }
-  
 }
 
